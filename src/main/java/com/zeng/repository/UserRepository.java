08334f95cd1fe7fcf61cbecb8a24470bd921f790
Guannian Zeng <vgnzeng@gmail.com>
2018-09-02 10:45:41 -0700

    added test code to make sure data is saved to MongoDB

--- src/main/java/com/zeng/repository/UserRepository.java
+++ src/main/java/com/zeng/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.zeng.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Guan on 9/2/2018.
--- src/test/java/com/zeng/repository/UserEntityDaoTest.java
+++ src/test/java/com/zeng/repository/UserEntityDaoTest.java
@@ -3,5 +3,37 @@ package com.zeng.repository;
 /**
  * Created by Guan on 9/2/2018.
  */
+
+import com.zeng.entity.UserEntity;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DataJpaTest
 public class UserEntityDaoTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testSaveUser() throws Exception {
+        UserEntity user = new UserEntity();
+        user.setId("1");
+        user.setUserName("Guan");
+        user.setPassword("hi5566");
+        user.setEmail("vgnzeng@gmail.com");
+        user.setUserType("manage");
+        userRepository.save(user);
+        //Assert.assertEquals("manage", userRepository.findByUserName("Guan").getUserType());
+        //Assert.assertEquals(1, userRepository.count());
+
+    }
+
 }
+
