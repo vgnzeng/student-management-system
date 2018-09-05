b7336a91a3c17c3af347bb275190229bdb36c111
Guannian Zeng <vgnzeng@gmail.com>
2018-09-04 21:46:12 -0700

    added Controller method to save Student

--- src/main/java/com/zeng/web/UserController.java
+++ src/main/java/com/zeng/web/UserController.java
@@ -1,14 +1,19 @@
 package com.zeng.web;
 
 import com.zeng.entity.UserEntity;
+import com.zeng.param.UserParam;
 import com.zeng.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -23,6 +28,19 @@ public class UserController {
         model.addAttribute("users", users);
         logger.info("user list "+ users);
         return "user/list";
+    }
+
+    @RequestMapping("/add")
+    public String add(@Valid UserParam userParam, BindingResult result, ModelMap model) {
+        UserEntity user=new UserEntity();
+        user.setUserName(userParam.getUserName());
+        user.setPassword(userParam.getPassword());
+        user.setEmail(userParam.getEmail());
+        user.setAge(userParam.getAge());
+        user.setRegTime(new Date());
+        user.setUserType("user");
+        userRepository.save(user);
+        return "redirect:/list";
     }
 
 }
