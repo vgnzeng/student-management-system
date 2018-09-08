6d5ac1745a9b90591ea48e643489313824c40285
Guannian Zeng <vgnzeng@gmail.com>
2018-09-07 23:25:27 -0700

    changed to Spring Boot 1.5.8 for fixing bug

--- src/main/java/com/zeng/StudentManagementSystemApplication.java
+++ src/main/java/com/zeng/StudentManagementSystemApplication.java
@@ -3,8 +3,7 @@ package com.zeng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class StudentManagementSystemApplication extends SpringBootServletInitializer {
--- src/main/java/com/zeng/web/UserController.java
+++ src/main/java/com/zeng/web/UserController.java
@@ -7,16 +7,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
+
 
 @Controller
 public class UserController {
@@ -25,10 +32,14 @@ public class UserController {
     private UserRepository userRepository;
 
     @RequestMapping("/list")
-    public String list(Model model) {
-        List<UserEntity> users=userRepository.findAll();
+    @Cacheable(value="user_list")
+    public String list(Model model,@RequestParam(value = "page", defaultValue = "0") Integer page,
+                       @RequestParam(value = "size", defaultValue = "6") Integer size) {
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(page, size, sort);
+        Page<UserEntity> users=userRepository.findAll(pageable);
         model.addAttribute("users", users);
-        logger.info("user list "+ users);
+        logger.info("user list "+ users.getContent());
         return "user/list";
     }
 
@@ -68,5 +79,29 @@ public class UserController {
         return "user/userEdit";
     }
 
+    @RequestMapping("/edit")
+    public String edit(@Valid UserParam userParam, BindingResult result,ModelMap model) {
+        String errorMsg="";
+        if(result.hasErrors()) {
+            List<ObjectError> list = result.getAllErrors();
+            for (ObjectError error : list) {
+                errorMsg=errorMsg + error.getCode() + "-" + error.getDefaultMessage() +";";
+            }
+            model.addAttribute("errorMsg",errorMsg);
+            model.addAttribute("user", userParam);
+            return "user/userEdit";
+        }
 
+        UserEntity user=userRepository.findById(userParam.getId());
+        BeanUtils.copyProperties(userParam,user);
+        user.setRegTime(new Date());
+        userRepository.save(user);
+        return "redirect:/list";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(String id) {
+        userRepository.deleteById(id);
+        return "redirect:/list";
+    }
 }
