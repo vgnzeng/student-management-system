8ca141b47b34694b9c700e607cf11f2190d9295a
Guannian Zeng <vgnzeng@gmail.com>
2018-09-07 22:37:16 -0700

    added data access layer CRUD method

--- src/main/java/com/zeng/repository/UserRepository.java
+++ src/main/java/com/zeng/repository/UserRepository.java
@@ -4,14 +4,15 @@ import com.zeng.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by Guan on 9/2/2018.
  */
 public interface UserRepository  extends MongoRepository<UserEntity, String> {
+    Page<UserEntity> findAll(Pageable pageable);
+    UserEntity findById(String id);
     UserEntity findByUserNameOrEmail(String userName,String email);
     UserEntity findByUserName(String userName);
     UserEntity findByEmail(String email);
-    
+    Long deleteById(String id);
 }
