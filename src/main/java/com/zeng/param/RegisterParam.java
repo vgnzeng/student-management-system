515a92152ab22d80527f37666125ef3226db86fe
Guannian Zeng <vgnzeng@gmail.com>
2018-09-11 10:17:12 -0700

    Updated POM fil

---
+++ src/main/java/com/zeng/param/RegisterParam.java
@@ -0,0 +1,46 @@
+package com.zeng.param;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+public class RegisterParam {
+    @NotEmpty(message="Name can't be empty")
+    private String userName;
+    @NotEmpty(message="Password can't be empty")
+    @Length(min=6,message="Password must be more than 6 digits")
+    private String password;
+    @Email
+    private String email;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
